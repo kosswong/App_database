becb0c0215b3dbbfdeecfa475d74a5c167bcee75
Koss Wong <33596596+kosswong@users.noreply.github.com>
2017-11-24 13:50:28 +0800

    test
    Test

--- application/views/main.php
+++ application/views/main.php
@@ -20,7 +20,7 @@
 	
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p class="footer">Testestsetsetstes! Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
 <?php include "footer.php"; ?>
