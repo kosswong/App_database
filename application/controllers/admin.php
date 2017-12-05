e0b8086f748efbd7fdd6670577886a29e278f8a4
phleung24 <33750605+phleung24@users.noreply.github.com>
2017-12-06 01:16:50 +0800

    20171206
    1:17

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -109,4 +109,82 @@ class admin extends CI_Controller {
       $this->load->view('admin_session', $data);
       $this->load->view('admin_footer', $data);
     }
+
+    public function trainer(){
+      $this->load->database();
+      $page = isset($_GET["page"]) ? $_GET["page"] : 1;
+      $results_per_page = 10;
+      $link = 7;
+      $start_from = ($page-1) * $results_per_page;
+
+      $sql_string = "SELECT t.* "
+          . "FROM trainers AS t "
+          . "ORDER BY t.trainer_id DESC "
+          . "LIMIT $start_from, $results_per_page";
+      $sql_query = $this->db->query($sql_string);
+
+      $result = $this->db->query("SELECT COUNT(trainer_id) AS total FROM trainers;");
+      $row = $result->row();
+
+      $data["total_pages"] = ceil($row->total / $results_per_page);
+      $data["current_page"] = $page;
+      $data["query_listing"] = $sql_query;
+
+      $this->load->view('admin_header', $data);
+      $this->load->view('admin_nav', $data);
+      $this->load->view('admin_trainer', $data);
+      $this->load->view('admin_footer', $data);
+    }
+
+    public function booking(){
+      $this->load->database();
+      $page = isset($_GET["page"]) ? $_GET["page"] : 1;
+      $results_per_page = 10;
+      $link = 7;
+      $start_from = ($page-1) * $results_per_page;
+
+      $sql_string = "SELECT b.* "
+          . "FROM bookings AS b "
+          . "ORDER BY b.booking_id DESC "
+          . "LIMIT $start_from, $results_per_page";
+      $sql_query = $this->db->query($sql_string);
+
+      $result = $this->db->query("SELECT COUNT(booking_id) AS total FROM bookings;");
+      $row = $result->row();
+
+      $data["total_pages"] = ceil($row->total / $results_per_page);
+      $data["current_page"] = $page;
+      $data["query_listing"] = $sql_query;
+
+      $this->load->view('admin_header', $data);
+      $this->load->view('admin_nav', $data);
+      $this->load->view('admin_booking', $data);
+      $this->load->view('admin_footer', $data);
+    }
+
+    public function venue(){
+      $this->load->database();
+      $page = isset($_GET["page"]) ? $_GET["page"] : 1;
+      $results_per_page = 10;
+      $link = 7;
+      $start_from = ($page-1) * $results_per_page;
+
+      $sql_string = "SELECT v.* "
+          . "FROM venues AS v "
+          . "ORDER BY v.venue_id DESC "
+          . "LIMIT $start_from, $results_per_page";
+      $sql_query = $this->db->query($sql_string);
+
+      $result = $this->db->query("SELECT COUNT(venue_id) AS total FROM venues;");
+      $row = $result->row();
+
+      $data["total_pages"] = ceil($row->total / $results_per_page);
+      $data["current_page"] = $page;
+      $data["query_listing"] = $sql_query;
+
+      $this->load->view('admin_header', $data);
+      $this->load->view('admin_nav', $data);
+      $this->load->view('admin_venue', $data);
+      $this->load->view('admin_footer', $data);
+    }
 }
