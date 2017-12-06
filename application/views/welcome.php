05f5e6c408a6d6b29d22d5c7b2a98e0e835541b9
Koss Wong <33596596+kosswong@users.noreply.github.com>
2017-12-06 18:39:48 +0800

    temp

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,7 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Wong
- * Date: 6/12/2017
- * Time: 18:33
- */
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="<?php echo $this->config->config['base_url']; ?>/images/banner1.jpg">
+        </div>
+        <div class="item">
+            <img src="<?php echo $this->config->config['base_url']; ?>/images/banner3.jpg">
+        </div>
+    </div>
+
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
